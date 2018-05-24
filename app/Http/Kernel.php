63fcde8cadac24b3000bfd51a928caca9117b939
mtvbrianking <mtvbrianking@gmail.com>
2018-05-24 14:41:20 +0300

    Create admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'admin.auth' => \App\Http\Middleware\RedirectIfNotAdmin::class,
+        'admin.guest' => \App\Http\Middleware\RedirectIfAdmin::class,
     ];
 }
