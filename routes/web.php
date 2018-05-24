aa832b0a1b367d9bd30889cad6f1e7a4e66f7d60
mtvbrianking <mtvbrianking@gmail.com>
2018-05-25 01:15:46 +0300

    Add Reset forgotten passord

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,10 @@ Route::group(['namespace'=>'Admin', 'prefix' => 'admin'], function() {
     Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('admin.register');
     Route::post('register', 'Auth\RegisterController@register');
 
+    // Passwords
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('admin.password.reset');
+
 });
