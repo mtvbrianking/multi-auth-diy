707a73005d84f6b6367842c2dcdd46b15d07376e
mtvbrianking <mtvbrianking@gmail.com>
2018-05-25 23:19:38 +0300

    Fix: reset forgot password

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -28,7 +28,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ url('/admin') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
