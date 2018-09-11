59e6e92a57386743a71c08c6506c8800d50c10fe
mtvbrianking <mtvbrianking@gmail.com>
2018-09-11 20:31:50 +0300

    Differentiate password reset tables per guard

--- config/auth.php
+++ config/auth.php
@@ -111,7 +111,7 @@ return [
 
         'admins' => [
             'provider' => 'admins',
-            'table' => 'password_resets',
+            'table' => 'admin_password_resets',
             'expire' => 60,
         ],
         
