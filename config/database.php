c48d31a296df37fb897a3e6b86f5458f7873de7e
mtvbrianking <mtvbrianking@gmail.com>
2022-05-15 09:39:05 +0300

    Debug GH Actions #1

--- config/database.php
+++ config/database.php
@@ -110,27 +110,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
-        'travis_ci' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => '127.0.0.1',
-            'port' => 3306,
-            'database' => 'travis_mysql_test_db',
-            'username' => 'root',
-            'password' => '',
-            'unix_socket' => '',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
     ],
 
     /*
