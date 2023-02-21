7c46c45504456037efec606d4f70a0445c766b38
mtvbrianking <mtvbrianking@gmail.com>
2023-02-21 13:28:52 +0300

    Improve test coverage

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -2,11 +2,20 @@
 
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Str;
 
 test('login screen can be rendered', function () {
     $response = $this->get('/login');
 
     $response->assertStatus(200);
+
+    // ...
+
+    $user = User::factory()->create(['email_verified_at' => now()]);
+
+    $response = $this->actingAs($user)->get(route('login'));
+
+    $response->assertRedirect(RouteServiceProvider::HOME);
 });
 
 test('users can authenticate using the login screen', function () {
@@ -29,5 +38,42 @@ test('users can not authenticate with invalid password', function () {
         'password' => 'wrong-password',
     ]);
 
+    $this->assertGuest();
+});
+
+test('can logout if authenticated', function () {
+    $this->be(User::factory()->create());
+
+    $response = $this->post(route('logout'));
+
+    $response->assertRedirect(url('/'));
+    $this->assertGuest();
+});
+
+test('can not make more than five failed login attempts a minute', function () {
+    $user = User::factory()->create();
+
+    foreach (range(0, 5) as $_) {
+        $response = $this->from(route('login'))->post(route('login'), [
+            'email' => $user->email,
+            'password' => Str::random(10),
+        ]);
+    }
+
+    $response->assertRedirect(route('login'));
+    $response->assertSessionHasErrors('email');
+    static::assertStringContainsString(
+        'Too many login attempts.',
+        collect(
+            $response
+                ->baseResponse
+                ->getSession()
+                ->get('errors')
+                ->getBag('default')
+                ->get('email')
+        )->first()
+    );
+    static::assertTrue(session()->hasOldInput('email'));
+    static::assertFalse(session()->hasOldInput('password'));
     $this->assertGuest();
 });
--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -3,51 +3,90 @@
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\URL;
 
 test('email verification screen can be rendered', function () {
-    $user = User::factory()->create([
-        'email_verified_at' => null,
-    ]);
+    $verifiedUser = User::factory()->create(['email_verified_at' => now()]);
 
-    $response = $this->actingAs($user)->get('/verify-email');
+    $response = $this->actingAs($verifiedUser)->get('/verify-email');
+
+    $response->assertRedirect(RouteServiceProvider::HOME);
+
+    // ...
+
+    $nonVerifiedUser = User::factory()->create(['email_verified_at' => null]);
+
+    $response = $this->actingAs($nonVerifiedUser)->get('/verify-email');
 
     $response->assertStatus(200);
 });
 
 test('email can be verified', function () {
-    $user = User::factory()->create([
-        'email_verified_at' => null,
-    ]);
+    $nonVerifiedUser = User::factory()->create(['email_verified_at' => null]);
+
+    $verifiedUser = User::factory()->create(['email_verified_at' => now()]);
 
     Event::fake();
 
-    $verificationUrl = URL::temporarySignedRoute(
-        'verification.verify',
-        now()->addMinutes(60),
-        ['id' => $user->id, 'hash' => sha1($user->email)]
-    );
+    $verificationUrl = URL::temporarySignedRoute('verification.verify', now()->addMinutes(60), [
+        'id' => $verifiedUser->id,
+        'hash' => sha1($verifiedUser->email),
+    ]);
 
-    $response = $this->actingAs($user)->get($verificationUrl);
+    $response = $this->actingAs($verifiedUser)->get($verificationUrl);
+
+    Event::assertNotDispatched(Verified::class);
+
+    $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+
+    // ...
+
+    $verificationUrl = URL::temporarySignedRoute('verification.verify', now()->addMinutes(60), [
+        'id' => $nonVerifiedUser->id,
+        'hash' => sha1($nonVerifiedUser->email),
+    ]);
+
+    $response = $this->actingAs($nonVerifiedUser)->get($verificationUrl);
 
     Event::assertDispatched(Verified::class);
-    expect($user->fresh()->hasVerifiedEmail())->toBeTrue();
+    expect($nonVerifiedUser->fresh()->hasVerifiedEmail())->toBeTrue();
     $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
 });
 
 test('email is not verified with invalid hash', function () {
-    $user = User::factory()->create([
-        'email_verified_at' => null,
-    ]);
+    $user = User::factory()->create(['email_verified_at' => null]);
 
-    $verificationUrl = URL::temporarySignedRoute(
-        'verification.verify',
-        now()->addMinutes(60),
-        ['id' => $user->id, 'hash' => sha1('wrong-email')]
-    );
+    $verificationUrl = URL::temporarySignedRoute('verification.verify', now()->addMinutes(60), [
+        'id' => $user->id,
+        'hash' => sha1('wrong-email'),
+    ]);
 
     $this->actingAs($user)->get($verificationUrl);
 
     expect($user->fresh()->hasVerifiedEmail())->toBeFalse();
+});
+
+test('resends email verification link', function () {
+    $verifiedUser = User::factory()->create(['email_verified_at' => now()]);
+
+    $response = $this->actingAs($verifiedUser)->post(route('verification.send'));
+
+    $response->assertRedirect(RouteServiceProvider::HOME);
+
+    // ...
+
+    Notification::fake();
+
+    $nonVerifiedUser = User::factory()->create(['email_verified_at' => null]);
+
+    $response = $this->actingAs($nonVerifiedUser)
+        ->from(route('verification.notice'))
+        ->post(route('verification.send'));
+
+    Notification::assertSentTo($nonVerifiedUser, VerifyEmail::class);
+
+    $response->assertRedirect(route('verification.notice'));
 });
